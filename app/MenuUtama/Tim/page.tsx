62208c0d1ed0c1dd91253ef78e48f3654d299cb8
IlhamMuhKofa <168963153+IlhamMuhKofa@users.noreply.github.com>
2025-01-06 14:57:16 +0700

    Update page.tsx

--- app/MenuUtama/Tim/page.tsx
+++ app/MenuUtama/Tim/page.tsx
@@ -2,81 +2,6 @@
 import React, { useState } from 'react';
 
 const teamData = {
-  teamCategories:
-    ['Tim Produksi',
-      'Tim Accounting',
-      'Tim IT',
-      'Tim Sales',
-      'Tim Gudang',
-      'Tim Satpam',
-      'Tim Supervisor',
-      'Tim Tambahan 1'
-    ], // Kategori hanya untuk "Team Lainnya"
-  members: [
-    //tim produksi
-    { name: 'Sophia Lee', position: 'UI/UX Designer', category: 'Tim Produksi', imageUrl: '/tim/tim1.jpg' },
-    { name: 'Michael Tan', position: 'Software Engineer', category: 'Tim Produksi', imageUrl: '/tim/tim2.jpeg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Produksi', imageUrl: '/tim/tim3.1.jpg' },
-    { name: 'Daniel Kim', position: 'Product Manager', category: 'Tim Produksi', imageUrl: '/tim/tim4.jpg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Produksi', imageUrl: '/tim/tim3.1.jpg' },
-    { name: 'Sophia Lee', position: 'UI/UX Designer', category: 'Tim Produksi', imageUrl: '/tim/tim1.jpg' },
-    { name: 'Michael Tan', position: 'Software Engineer', category: 'Tim Produksi', imageUrl: '/tim/tim2.jpeg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Produksi', imageUrl: '/tim/tim3.1.jpg' },
-    { name: 'Daniel Kim', position: 'Product Manager', category: 'Tim Produksi', imageUrl: '/tim/tim4.jpg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Produksi', imageUrl: '/tim/tim3.1.jpg' },
-    // Tambahkan lebih banyak anggota tim di sini
-    //tim accounting
-    { name: 'Sophia Lee', position: 'UI/UX Designer', category: 'Tim Accounting', imageUrl: '/tim/tim1.jpg' },
-    { name: 'Michael Tan', position: 'Software Engineer', category: 'Tim Accounting', imageUrl: '/tim/tim2.jpeg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Accounting', imageUrl: '/tim/tim3.1.jpg' },
-    { name: 'Daniel Kim', position: 'Product Manager', category: 'Tim Accounting', imageUrl: '/tim/tim4.jpg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Accounting', imageUrl: '/tim/tim3.1.jpg' },
-    //tim it
-    { name: 'Sophia Lee', position: 'UI/UX Designer', category: 'Tim IT', imageUrl: '/tim/tim1.jpg' },
-    { name: 'Michael Tan', position: 'Software Engineer', category: 'Tim IT', imageUrl: '/tim/tim2.jpeg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim IT', imageUrl: '/tim/tim3.1.jpg' },
-    { name: 'Daniel Kim', position: 'Product Manager', category: 'Tim IT', imageUrl: '/tim/tim4.jpg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim IT', imageUrl: '/tim/tim3.1.jpg' },
-    { name: 'Sophia Lee', position: 'UI/UX Designer', category: 'Tim IT', imageUrl: '/tim/tim1.jpg' },
-    { name: 'Michael Tan', position: 'Software Engineer', category: 'Tim IT', imageUrl: '/tim/tim2.jpeg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim IT', imageUrl: '/tim/tim3.1.jpg' },
-    { name: 'Daniel Kim', position: 'Product Manager', category: 'Tim IT', imageUrl: '/tim/tim4.jpg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim IT', imageUrl: '/tim/tim3.1.jpg' },
-    //tim sales
-    { name: 'Sophia Lee', position: 'UI/UX Designer', category: 'Tim Sales', imageUrl: '/tim/tim1.jpg' },
-    { name: 'Michael Tan', position: 'Software Engineer', category: 'Tim Sales', imageUrl: '/tim/tim2.jpeg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Sales', imageUrl: '/tim/tim3.1.jpg' },
-    { name: 'Daniel Kim', position: 'Product Manager', category: 'Tim Sales', imageUrl: '/tim/tim4.jpg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Sales', imageUrl: '/tim/tim3.1.jpg' },
-    //tim gudang
-    { name: 'Sophia Lee', position: 'UI/UX Designer', category: 'Tim Gudang', imageUrl: '/tim/tim1.jpg' },
-    { name: 'Michael Tan', position: 'Software Engineer', category: 'Tim Gudang', imageUrl: '/tim/tim2.jpeg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Gudang', imageUrl: '/tim/tim3.1.jpg' },
-    { name: 'Daniel Kim', position: 'Product Manager', category: 'Tim Gudang', imageUrl: '/tim/tim4.jpg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Gudang', imageUrl: '/tim/tim3.1.jpg' },
-    { name: 'Sophia Lee', position: 'UI/UX Designer', category: 'Tim Gudang', imageUrl: '/tim/tim1.jpg' },
-    { name: 'Michael Tan', position: 'Software Engineer', category: 'Tim Gudang', imageUrl: '/tim/tim2.jpeg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Gudang', imageUrl: '/tim/tim3.1.jpg' },
-    { name: 'Daniel Kim', position: 'Product Manager', category: 'Tim Gudang', imageUrl: '/tim/tim4.jpg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Gudang', imageUrl: '/tim/tim3.1.jpg' },
-    //tim satpam
-    { name: 'Sophia Lee', position: 'UI/UX Designer', category: 'Tim Satpam', imageUrl: '/tim/tim1.jpg' },
-    { name: 'Michael Tan', position: 'Software Engineer', category: 'Tim Satpam', imageUrl: '/tim/tim2.jpeg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Satpam', imageUrl: '/tim/tim3.1.jpg' },
-    { name: 'Daniel Kim', position: 'Product Manager', category: 'Tim Satpam', imageUrl: '/tim/tim4.jpg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Satpam', imageUrl: '/tim/tim3.1.jpg' },
-    //tim supervisor
-    { name: 'Sophia Lee', position: 'UI/UX Designer', category: 'Tim Supervisor', imageUrl: '/tim/tim1.jpg' },
-    { name: 'Michael Tan', position: 'Software Engineer', category: 'Tim Supervisor', imageUrl: '/tim/tim2.jpeg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Supervisor', imageUrl: '/tim/tim3.1.jpg' },
-    { name: 'Daniel Kim', position: 'Product Manager', category: 'Tim Supervisor', imageUrl: '/tim/tim4.jpg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Supervisor', imageUrl: '/tim/tim3.1.jpg' },
-    { name: 'Sophia Lee', position: 'UI/UX Designer', category: 'Tim Supervisor', imageUrl: '/tim/tim1.jpg' },
-    { name: 'Michael Tan', position: 'Software Engineer', category: 'Tim Supervisor', imageUrl: '/tim/tim2.jpeg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Supervisor', imageUrl: '/tim/tim3.1.jpg' },
-    { name: 'Daniel Kim', position: 'Product Manager', category: 'Tim Supervisor', imageUrl: '/tim/tim4.jpg' },
-    { name: 'Emily Chen', position: 'Marketing Specialist', category: 'Tim Supervisor', imageUrl: '/tim/tim3.1.jpg' },
-  ],
   ceo: {
     name: 'H. Tito Idroes S.Sos.',
     position: 'CEO & Founder',
@@ -89,14 +14,19 @@ const teamData = {
     { name: 'Dexy Alfaro S.Tr.Kom.', position: 'HRD Manager', imageUrl: '/tim/Dexy.jpg' },
     { name: 'Ilham Muh Kofa S.Tr.Kom.', position: 'HR Manager', imageUrl: '/tim/Ilham.jpg' },
   ],
+  otherTeam: [
+    { name: '1', imageUrl: '/tim/tim1.jpg' },
+    { name: '2', imageUrl: '/tim/tim2.jpeg' },
+    { name: '3', imageUrl: '/tim/tim3.1.jpg' },
+    { name: '4', imageUrl: '/tim/tim3.jpeg' },
+    { name: '5', imageUrl: '/tim/tim4.jpg' },
+    { name: '6', imageUrl: '/tim/tim5.jpg' },
+    { name: '7', imageUrl: '/tim/tim1.jpg' },
+    { name: '8', imageUrl: '/tim/tim3.1.jpg' },
+  ],
 };
 
 const TeamPage: React.FC = () => {
-  const [selectedCategory, setSelectedCategory] = useState(teamData.teamCategories[0]);
-
-  // Filter anggota berdasarkan kategori
-  const filteredMembers = teamData.members.filter((member) => member.category === selectedCategory);
-
   return (
     <div className="container mx-auto px-4 my-12 space-y-8">
       {/* Header */}
@@ -150,35 +80,20 @@ const TeamPage: React.FC = () => {
         <div className="text-center mb-6">
           <h2 className="text-4xl font-bold text-gray-800">Team Lainnya</h2>
         </div>
-
-        {/* Kategori Tabs */}
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:flex lg:justify-center lg:space-x-4 gap-4 mb-8">
-          {teamData.teamCategories.map((category) => (
-            <button
-              key={category}
-              onClick={() => setSelectedCategory(category)}
-              className={`px-4 py-2 rounded-full font-medium ${selectedCategory === category
-                  ? 'bg-blue-500 text-white'
-                  : 'bg-gray-200 text-gray-700 hover:bg-gray-300'
-                }`}
-            >
-              {category}
-            </button>
-          ))}
-        </div>
-
-        {/* Anggota Tim Lainnya */}
-        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-5 gap-4">
-          {filteredMembers.map((member, index) => (
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+          {teamData.otherTeam.map((member, index) => (
             <div
               key={index}
-              className="text-center bg-white shadow-md rounded-lg p-2 transform hover:scale-105 transition"
+              className="relative group bg-gray-100 rounded-lg shadow-lg overflow-hidden"
             >
-              <div className="w-20 h-20 mx-auto rounded-full overflow-hidden mb-2">
-                <img src={member.imageUrl} alt={member.name} className="w-full h-full object-cover" />
+              <img
+                src={member.imageUrl}
+                alt={member.name}
+                className="w-full h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
+              />
+              <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <span className="text-white text-lg font-semibold">{member.name}</span>
               </div>
-              <h3 className="text-sm font-semibold">{member.name}</h3>
-              <p className="text-gray-600 text-xs">{member.position}</p>
             </div>
           ))}
         </div>
