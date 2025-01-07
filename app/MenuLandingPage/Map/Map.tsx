1f0a3fa7e90ebf0987d58704b7ffa6e66a28d0e0
IlhamMuhKofa <168963153+IlhamMuhKofa@users.noreply.github.com>
2025-01-07 08:59:03 +0700

    Update Map.tsx

--- app/MenuLandingPage/Map/Map.tsx
+++ app/MenuLandingPage/Map/Map.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
@@ -12,22 +12,19 @@ const Map: React.FC = () => {
       .then((response) => response.json())
       .then((data) => setMarkers(data))
       .catch((error) => console.error("Error loading JSON:", error));
-
-    // Cleanup function for map when component unmounts
-    return () => {
-      // Optional cleanup (depends on Leaflet's behavior in Next.js)
-    };
   }, []);
 
   return (
-    <div className="container mx-auto p-4">
-      <div className="bg-sky-500 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 overflow-hidden">
+    <div className="container mx-auto p-8">
+      <div className="bg-sky-500 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 overflow-hidden mt-16">
+        {/* Header */}
         <div className="p-6 border-b text-center flex flex-col justify-center items-center">
           <h2 className="text-3xl font-bold text-white mb-4">Persebaran Produk</h2>
           <p className="text-white">Berikut adalah peta persebaran produk kami di wilayah Sumatra Barat.</p>
         </div>
 
-        <div className="map-container">
+        {/* Map Container */}
+        <div className="map-container" style={{ height: '500px', position: 'relative', zIndex: 1 }}>
           <MapContainer
             center={[0.9471, 100.4172]}
             zoom={7}
@@ -42,7 +39,11 @@ const Map: React.FC = () => {
               <Marker
                 key={marker.id}
                 position={marker.position}
-                icon={marker.icon ? L.icon({ iconUrl: marker.icon, iconSize: [50, 50], iconAnchor: [16, 32], popupAnchor: [0, -32] }) : undefined}
+                icon={
+                  marker.icon
+                    ? L.icon({ iconUrl: marker.icon, iconSize: [50, 50], iconAnchor: [16, 32], popupAnchor: [0, -32] })
+                    : undefined
+                }
               >
                 <Popup>{marker.name}</Popup>
               </Marker>
